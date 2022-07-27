6a1a4ed306cc07b92183c8a226eeab8a7f02b727
Michał Winnicki <wmichal13@wp.pl>
2022-07-27 12:44:32 +0000

    Q-001_todo- rethink answer display in Result

--- src/result.jsx
+++ src/result.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import userAnswers from './user_answers';
+import questions from './questions';
 
 const Result = (props) => {
+  let userChoices = userAnswers.map((choice, index) => {
+    console.log("index: " + index + " | choice:" + choice + " | " + questions[index].choices[choice].isCorrect)
+    if(questions[index].choices[choice].isCorrect){
+      console.log("bingo");
+    return <li key={index}>
+      <h3>{questions[index].question}</h3>
+        <p className="right-answer">
+          {questions[index].choices[choice].answer}
+        </p>
+      </li>
+    
+    }else{
+      return <li key={index}>
+        <h3>{questions[index].question}</h3>
+        <p className="wrong-answer">
+        {questions[index].choices[choice].answer}
+      </p>
+        </li>
+    }
+  }
+    );
   return (
     <div id="result-container">
       <h1 id="result-header">Your points</h1>
       <h2>{props.points + "/" + props.maxRange}</h2>
+      <ul>{userChoices}</ul>
     </div>
   )
 }
