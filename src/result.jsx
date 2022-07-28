15467e9226c2e01faf2e22e9343314ef58d3bded
Michał Winnicki <wmichal13@wp.pl>
2022-07-28 10:33:38 +0000

    functionality finished, todo rewrite questions to Java

--- src/result.jsx
+++ src/result.jsx
@@ -3,31 +3,28 @@ import userAnswers from './user_answers';
 import questions from './questions';
 
 const Result = (props) => {
-  let userChoices = userAnswers.map((choice, index) => {
-    console.log("index: " + index + " | choice:" + choice + " | " + questions[index].choices[choice].isCorrect)
-    if(questions[index].choices[choice].isCorrect){
-      console.log("bingo");
+  let correctAnswer = "bingo";
+  let userChoices = userAnswers.map((choice, index) => {    
+    if(!questions[index].choices[choice].isCorrect){
+      for (let i = 0; i < questions[index].choices.length; i++) {
+        if(questions[index].choices[i].isCorrect){
+          correctAnswer = questions[index].choices[i].answer;
+      }      
+    }
     return <li key={index}>
       <h3>{questions[index].question}</h3>
-        <p className="right-answer">
+        <p className="wrong-answer">
           {questions[index].choices[choice].answer}
         </p>
+      <p className="right-answer">{correctAnswer}</p>
       </li>
-    
-    }else{
-      return <li key={index}>
-        <h3>{questions[index].question}</h3>
-        <p className="wrong-answer">
-        {questions[index].choices[choice].answer}
-      </p>
-        </li>
-    }
+     }
   }
     );
   return (
     <div id="result-container">
-      <h1 id="result-header">Your points</h1>
-      <h2>{props.points + "/" + props.maxRange}</h2>
+      <h1 id="result-header">You got {props.points + "/" + props.maxRange} answers right.</h1>
+      <h2>Wrong answers:</h2>
       <ul>{userChoices}</ul>
     </div>
   )
